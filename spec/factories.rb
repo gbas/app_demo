44fe5c02f6a25865bd27a2b8f3b3c2f885569552
gbas <codewinphone7@live.com>
2012-10-10 20:24:18 +0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+
+	factory :user do
+	
+		name     "George Basagiannis"
+		
+		email    "gbas@live.com"
+		
+		password "foobar"
+		
+		password_confirmation "foobar"
+		
+	end
+
+end
+
