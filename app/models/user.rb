831c06e62599998dbbcb1ec17b8435c5e0d90738
gbas <codewinphone7@live.com>
2012-10-10 23:37:12 +0300

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
 	
 	before_save { |user| user.email = email.downcase }
 
+	before_save :create_remember_token
 
 	
 	validates :name, presence: true, length: { maximum: 50 }
@@ -32,4 +33,12 @@ class User < ActiveRecord::Base
 	validates :password, presence: true, length: { minimum: 6 }
 	
 	validates :password_confirmation, presence: true
+
+
+	   
+	def create_remember_token
+	
+		self.remember_token = SecureRandom.urlsafe_base64
+		
+	end
 end
